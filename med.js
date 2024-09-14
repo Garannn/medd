0ed8294021e84923cfc2df4c422901a6b71170fa
Garannn <151853944+Garannn@users.noreply.github.com>
2024-09-14 10:41:59 +0300

    Add files via upload

---
+++ med.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  Menu.setApplicationMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
